03900b0870e5136e3afb29ac9b607cfd04a31871
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-03-14 14:12:50 +0200

    Add options inputs and handled it

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import "./style.css";
+import getAllInputData from "./views/menuView.js";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
+
+document.querySelector("#start-button").addEventListener("click", () => {
+  console.log(getAllInputData());
+});
---
+++ views/menuView.js
@@ -0,0 +1,55 @@
+import appConstants from "../appConstants.js";
+const widthInput = document.querySelector("#width-input");
+const heightInput = document.querySelector("#height-input");
+const heuristicFunctionsRadioButtons = [
+  document.querySelector("#manhattan-radio-input"),
+  document.querySelector("#ecludian-radio-input"),
+];
+const speedRangeInput = document.querySelector("#speed-range-input");
+
+const getAllInputData = () => {
+  return {
+    boardWidth: getWidthInputValue(),
+    boardHeight: getHeightInputValue(),
+    heuristicFunction: getHeuristicInputValue(),
+    speed: getSpeedRangeInputValue(),
+  };
+};
+
+const getWidthInputValue = () => {
+  let inputWidthValue = +widthInput.value || appConstants.defaultBoardWidth;
+
+  if (inputWidthValue > appConstants.maxBoardWidth) {
+    inputWidthValue = appConstants.maxBoardWidth;
+  }
+  if (inputWidthValue < appConstants.minBoardWidth) {
+    inputWidthValue = appConstants.minBoardWidth;
+  }
+  widthInput.value = inputWidthValue;
+  return inputWidthValue;
+};
+
+const getHeightInputValue = () => {
+  let inputHeightValue = +heightInput.value || appConstants.defaultBoardHeight;
+
+  if (inputHeightValue > appConstants.maxBoardHeight) {
+    inputHeightValue = appConstants.maxBoardHeight;
+  }
+  if (inputHeightValue < appConstants.minBoardHeight) {
+    inputHeightValue = appConstants.minBoardHeight;
+  }
+  heightInput.value = inputHeightValue;
+  return inputHeightValue;
+};
+
+const getHeuristicInputValue = () => {
+  return heuristicFunctionsRadioButtons
+    .filter((h) => h.checked)
+    .map((h) => h.id)[0]
+    .split("-")[0];
+};
+const getSpeedRangeInputValue = () => {
+  const speed = +speedRangeInput.value / 25;
+  return speed;
+};
+export default getAllInputData;
